dffc860d83b2be2e95c751d1e117a86843e849e6
VilERP <ronny.pruebas123@gmail.com>
2025-10-20 17:43:05 -0500

    fix: Mover DocTypes SUNAT a /regional/doctype/ para compatibilidad
    - Movidos 3 DocTypes de /regional/peru/doctype/ a /regional/doctype/
      - SUNAT Tipo Documento Identidad (Tabla 2)
      - SUNAT Tipo Existencia (Tabla 5)
      - SUNAT Unidad Medida (Tabla 6)
    - Actualizado module de 'Regional - Peru' a 'Regional'
    - Corrige error de importación que impedía acceder a Sales Invoice
    - Sigue el estándar de ERPNext para DocTypes regionales
    - Los DocTypes permanecen en /regional/peru/doctype/:
      - SUNAT Configuration
      - SUNAT Document Type
      - SUNAT Payment Method

---
+++ erpnext/regional/doctype/sunat_tipo_documento_identidad/sunat_tipo_documento_identidad.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SUNAT Tipo Documento Identidad', {
+    refresh: function(frm) {
+    }
+});
---
+++ erpnext/regional/doctype/sunat_tipo_existencia/sunat_tipo_existencia.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SUNAT Tipo Existencia', {
+    refresh: function(frm) {
+    }
+});
